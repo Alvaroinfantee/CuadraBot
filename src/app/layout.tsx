0592b21cee838c35eb711eba056428c2bc22bc0e
Alvaroinfantee <Alvaroinfantee@gmail.com>
2026-06-15 09:18:31 +0200

    Load Google Ads tag earlier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { GoogleAdsTag } from "@/components/site/google-ads-tag";
+import Script from "next/script";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+
+const googleAdsId = process.env.NEXT_PUBLIC_GOOGLE_ADS_ID ?? "AW-18182187189";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,10 +37,26 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        {googleAdsId ? (
+          <>
+            <Script
+              id="google-ads-gtag-src"
+              src={`https://www.googletagmanager.com/gtag/js?id=${googleAdsId}`}
+              strategy="beforeInteractive"
+            />
+            <Script id="google-ads-gtag-init" strategy="beforeInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${googleAdsId}');
+              `}
+            </Script>
+          </>
+        ) : null}
         {children}
         <Toaster />
       </body>
-      <GoogleAdsTag />
     </html>
   );
 }
